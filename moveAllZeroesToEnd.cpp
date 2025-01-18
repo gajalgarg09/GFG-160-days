b7ae639a602debae3ba7f67e25b3f307e5059c41
Gajal Garg <gajalgarg09@gmail.com>
2025-01-19 00:58:18 +0530

    Add files via upload

---
+++ moveAllZeroesToEnd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        
+        int n = arr.size();
+        int j = 0;
+        for(int i =0; i<n; i++){
+            if(arr[i] != 0){
+                swap(arr[i] , arr[j]);
+                j++;
+            }
+        }
+    }
+};
