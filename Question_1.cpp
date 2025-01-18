ee2f6f6b6c59ec0cb718f70c9823ffef871383ca
Gajal Garg <gajalgarg09@gmail.com>
2025-01-19 01:07:13 +0530

    Update and rename moveAllZeroesToEnd.cpp to Question_1.cpp

--- Question_1.cpp
+++ Question_1.cpp
@@ -1,3 +1,7 @@
+// You are given an array arr[] of non-negative integers. Your task is to move all the zeros in the array to the right end 
+// while maintaining the relative order of the non-zero elements. The operation must be performed in place, 
+// meaning you should not use extra space for another array.
+
 #include<iostream>
 #include<vector>
 using namespace std;
