8f9515033019a63078e42f47a0deb2d695062dbf
Gajal Garg <gajalgarg09@gmail.com>
2025-01-19 22:26:42 +0530

    Add files via upload

---
+++ searchInRotatedSortedArray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+    public: 
+        int search(vector<int>& arr, int key) {
+        int n = arr.size();
+        
+        for(int i =0; i<n; i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
