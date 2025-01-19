d15d775834545fe176cce1fa6a3856897465c720
Gajal Garg <gajalgarg09@gmail.com>
2025-01-19 21:38:32 +0530

    Add files via upload

---
+++ numberOfOccurence.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+        int count = 0;
+        for(int i =0; i<arr.size(); i++){
+            if(arr[i] == target){
+                count++;
+            }
+        }
+        return count;
+    }
+};
