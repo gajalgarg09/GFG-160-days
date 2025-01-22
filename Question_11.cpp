e16b2c530098bd098d25d3330dbee097d7d41fb0
Gajal Garg <gajalgarg09@gmail.com>
2025-01-22 09:26:33 +0530

    Update Question_11.cpp

--- Question_11.cpp
+++ Question_11.cpp
@@ -1,5 +1,6 @@
 // You are given a positive integer n, you need to add all the digits of n and create a new number. 
-// Perform this operation until the resultant number has only one digit in it. Return the final number obtained after performing the given operation.
+// Perform this operation until the resultant number has only one digit in it. 
+// Return the final number obtained after performing the given operation.
 
 #include<iostream>
 #include<vector>
