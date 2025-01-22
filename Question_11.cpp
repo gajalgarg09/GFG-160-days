f59ad5788f1089813c97cb063e09487cd16563c9
Gajal Garg <gajalgarg09@gmail.com>
2025-01-22 09:26:16 +0530

    Update and rename repititiveAdditionOfDigits.cpp to Question_11.cpp

--- Question_11.cpp
+++ Question_11.cpp
@@ -1,3 +1,6 @@
+// You are given a positive integer n, you need to add all the digits of n and create a new number. 
+// Perform this operation until the resultant number has only one digit in it. Return the final number obtained after performing the given operation.
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -6,17 +9,15 @@ class solution{
     public:
         int singleDigit(int n) {
         
-        // Using direct formula
+        // Method - 1 (Using direct formula)
         if( n == 0){
             return 0;
         }
-        
         else{
             return ((n-1)%9) + 1;
         }
         
-        
-        // using loops
+        // Method - 2 (using loops)
         while( n >= 10){
             int sum =0;
             
