294baea5ed7e0c2e496e34ead854c8ddb9987a32
Gajal Garg <gajalgarg09@gmail.com>
2025-01-22 12:23:22 +0530

    Update and rename kth_smallest.cpp to Question_12.cpp

--- Question_12.cpp
+++ Question_12.cpp
@@ -1,3 +1,7 @@
+// Given an array arr[] and an integer k where k is smaller than the size of the array, 
+// the task is to find the kth smallest element in the given array.
+// Follow up: Don't solve it using the inbuilt sort function.
+
 #include<iostream>
 #include<vector>
 using namespace std;
